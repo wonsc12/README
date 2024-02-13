0f4f3ce9a58298f7ff161169758171612d7fd257
wonsc12 <turnapple123@naver.com>
2024-02-13 10:41:45 +0900

    bbs start

--- config.php
+++ config.php
@@ -3,7 +3,6 @@ $mysqli = mysqli_connect('localhost', 'simplebbs', '12345', 'simplebbs');
 
 if (mysqli_connect_errno()) {
  echo "DB연결에 실패했습니다.".mysqlii_connect_error();
-}else{
-  echo "DB연결 성공";
 }
+
 ?>
